a5c6f86db7e35d2238ea5457b4a70dc8d4a02ae7
Mohan <smrajcse@gmail.com>
2020-10-28 15:46:50 +0530

    Added class file

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vs_gitlearning
+{
+    class Class1
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace vs_gitlearning
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello GitHub!");
         }
     }
 }
