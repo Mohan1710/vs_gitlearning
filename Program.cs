bc6610ceb7954690271ac00e8516b08bd2cd68c5
Mohan <smrajcse@gmail.com>
2020-10-28 15:41:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vs_gitlearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
